1b2d6fa0ffc150958ab4e576b881da7db930219c
Paloma Mirta Mendez <mpaloma2014@gmail.com>
2020-08-01 03:28:36 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,10 +7,6 @@
 # }
 
 def select_winner(passengers)
-  winner = ""
-  passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
-  end
-end
+  # add the code snippet here!
+  
 end
